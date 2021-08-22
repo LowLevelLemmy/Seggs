65c75ce013db660d22ab9b28cca90aa9637e1d22
LowLevelLemmy <77054159+LowLevelLemmy@users.noreply.github.com>
2021-08-21 17:22:22 -0700

    Added IPHONE TIME. THAT REALLY HAS THE IRL TIME

---
+++ Seggs/Assets/Folders/Scripts/IphoneClock.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using EasyButtons;
+using TMPro;
+
+public class IphoneClock : MonoBehaviour
+{
+    TextMeshProUGUI txt;
+    void OnEnable()
+    {
+        txt = GetComponent<TextMeshProUGUI>();
+        txt.text = GetTimeStr();
+    }
+
+    [Button]
+    string GetTimeStr()
+    {
+        DateTime now = DateTime.Now;
+        string time = String.Format("{0:h:m}", now);
+        return time;
+    }
+}
